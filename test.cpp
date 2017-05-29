de8f00dd03a154e5c204e9fd190ce4e7f2bfc4fd
kovasky1 <kovaskybuezo@gmail.com>
2017-05-29 15:47:37 -0600

    Testing box() method

--- test.cpp
+++ test.cpp
@@ -2,15 +2,17 @@
 
 int main()
 {
-	cprintf("La fruta que te gusta es la: ");
-	list_radiobutton *list1 = new list_radiobutton;
+	//cprintf("La fruta que te gusta es la: ");
+	list_checkbox *list1 = new list_checkbox;
 	list1->add("manzana");
 	list1->add("naranja");
 	list1->add("melon");
 	list1->add("fresa");
-	list1->show(3,3,0);
-   list1->focus();
-   list1->result(3,7);
+
+	list1->box(2,1);
+   list1->ini->show(4,2);
+   //list1->focus();
+   //list1->result(3,7);
 
 	getch();
 
